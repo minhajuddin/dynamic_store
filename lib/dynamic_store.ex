43ff28744df5073e6b67c8aee4bef152d058c17d
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-08-07 15:00:32 +0530

    init

---
+++ lib/dynamic_store.ex
@@ -0,0 +1,18 @@
+defmodule DS do
+  @moduledoc """
+  Documentation for DS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DS.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
